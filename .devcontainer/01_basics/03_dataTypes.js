288f8667069c6a36d9131f7280888e6308c9c9ea
Pratik-tech-design <pverma050.btech2023@che.nitrr.ac.in>
2026-04-03 06:27:42 +0000

    output

--- .devcontainer/01_basics/03_dataTypes.js
+++ .devcontainer/01_basics/03_dataTypes.js
@@ -13,3 +13,26 @@ let isLoggedIn = 1 ;
 let stringIsLoggedIn = String(isLoggedIn);
 
 console.log(typeof (stringIsLoggedIn));
+
+// ****************** OPeRATORS IN JS ***********************
+/*
+let value = 3;
+let negValue = -value;
+
+console.log(negValue);
+*//*
+ let str1 = "Hello";
+ let str2 = " World";
+ let str3 = str1 + str2;
+    console.log(str3);*/
+
+// console.log(3 + 3);    
+// console.log(3 + "3");
+// console.log("3" + 4 + 5);
+// console.log(3 + 4 + "5");
+
+// console.log(true);
+let gamecounter = 100;
+gamecounter++;
+console.log(gamecounter);
+
