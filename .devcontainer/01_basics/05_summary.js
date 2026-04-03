531d259aa1b715a1dfe3a22afaaa6bdcfa337077
Pratik-tech-design <pverma050.btech2023@che.nitrr.ac.in>
2026-04-03 07:00:53 +0000

    Add summary of primitive and reference types in 05_summary.js

---
+++ .devcontainer/01_basics/05_summary.js
@@ -0,0 +1,29 @@
+// Primitives 
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt;
+
+const score = 100;
+const scoreValue = 100.5;
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail; // undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console. log(id === anotherId);
+
+const bigNumber = 123456789012345678901234567890n;
+
+
+
+// Reference / not primitive types : Object, Array, Function, 
+
+const heros = ["Shaktiman", "Bheem"];
+{
+    name : "Hitesh"
+    age : 24,
+}
+
+const mfunction = function() {
+    console.log("Hello world");
+}
